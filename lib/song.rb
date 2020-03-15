1dccf52fba90baa14a150d9b7cb57ac8dd20b84a
Amal Abdu <abduamal@ymail.com>
2020-03-15 20:29:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,17 @@
 class Song
+  @@count = 0
+  
   attr_accessor :name, :artist, :genre
+  
+  def initialize(song_name, song_artist, song_genre)
+    @name = song_name
+    @artist = song_artist
+    @genre = song_genre
+    
+    @@count += 1
+  end
+  
+  def count
+    @@count
+  end
 end
