10f118d825ee4ed1e614f4c537afd2fa2fd131f0
Amal Abdu <abduamal@ymail.com>
2020-03-15 20:43:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,15 @@ class Song
     @name = song_name
     @artist = song_artist
     @genre = song_genre
-    if !(@@genres.include?(@genre)
+    if !(@@genres.include?(@genre))
       @@genres << @genre
+    end
+    if !(@@artists.include?(@artist))
+      @@artists << @artist
     end
     @@count += 1
     return @@genres
+    return @@artists
   end
   
   def self.count
